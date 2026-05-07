fc13272c2af5256fb4d244786f596f2d8391ed82
nue-os <kimsoeun0108@gmail.com>
2026-05-07 18:14:54 +0900

    feat: 자격증 데이터 추가

---
+++ src/data/certificates.ts
@@ -0,0 +1,28 @@
+import type { Certificate } from '../types/certificate'
+
+export const certificates: Certificate[] = [
+  {
+    name: '정보처리기사',
+    issuer: '한국산업인력공단',
+    date: '2024.06',
+    icon: '🏆',
+  },
+  {
+    name: 'SQLD',
+    issuer: '한국데이터산업진흥원',
+    date: '2023.12',
+    icon: '🗄️',
+  },
+  {
+    name: '컴퓨터활용능력 1급',
+    issuer: '대한상공회의소',
+    date: '2023.04',
+    icon: '💻',
+  },
+  {
+    name: '자격증 이름',
+    issuer: '발급기관',
+    date: '2023.01',
+    icon: '📜',
+  },
+]
